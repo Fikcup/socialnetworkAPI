f92b83726c106defd4ede0574f22e56ac4597c89
Fikcup <rhys.n.wright@gmail.com>
2021-10-06 10:25:29 -0700

    feat : added GET ALL thoughts

---
+++ controllers/thought-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find()
+            .then((thoughtData) => {
+                res.json(thoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+}
+
+module.exports = thoughtController;
