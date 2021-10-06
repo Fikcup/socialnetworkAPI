a25c745fb341391beebb5de6a2ec9a33bfbec124
Fikcup <rhys.n.wright@gmail.com>
2021-10-05 23:17:58 -0700

    feat : added a reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Type } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: () => Date.now(),
+            // getter timestamp query
+        }
+    }
+)
+
+const Reaction = model ('Reaction', reactionSchema);
+
+module.exports = Reaction;
