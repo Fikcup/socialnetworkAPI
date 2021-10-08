53be6991821b6ad4b63eb003b66a2ec8b76f98fc
Fikcup <rhys.n.wright@gmail.com>
2021-10-08 09:09:01 -0700

    feat : remove friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -75,7 +75,7 @@ const userController = {
     createFriend(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
-            { $push: { friends: req.params.friendId } },
+            { $addToSet: { friends: req.params.friendId } },
             { new: true }
         )
             .then((userData) => {
@@ -84,6 +84,24 @@ const userController = {
                 }
 
                 res.json(userData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    },
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true },
+        )
+            .then((userData) => {
+                if (!userData) {
+                    return res.status(404).json({ message: 'No user exists with this id!' });
+                }
+
+                res.json({ message: 'Friend has been removed' });
             })
             .catch((err) => {
                 console.log(err);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,12 +6,13 @@ const {
     updateUser,
     deleteUser,
     createFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friend/:friendId').post(createFriend);
+router.route('/:userId/friend/:friendId').post(createFriend).delete(deleteFriend);
 
 module.exports = router;
